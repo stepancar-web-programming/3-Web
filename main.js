3bb570c17f5222abd739c2ae0826b83e54acb2ac
Nekruz Nazirjonov <92411934+nekruz03@users.noreply.github.com>
2024-01-14 02:06:57 +0300

    added linters

--- main.js
+++ main.js
@@ -1,53 +1,50 @@
 
-console.log("Hello from main.js!");
-
-document.addEventListener("DOMContentLoaded", function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const page = urlParams.get('page');
-
-    if (page === 'projects') {
-        const projectDetailsSection = document.getElementById('project-details');
-        projectDetailsSection.innerHTML += "<p>Additional content for projects page.</p>";
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const page = urlParams.get('page');
+  if (page === 'projects') {
+    const projectDetailsSection = document.getElementById('project-details');
+    projectDetailsSection.innerHTML += '<p>Additional content for projects page.</p>';
+  }
 });
 document.addEventListener('DOMContentLoaded', () => {
-    let nightMode = localStorage.getItem('nightMode');
-
-    if (nightMode === null) {
-        nightMode = 'false';
-    }
-
+  let nightMode = localStorage.getItem('nightMode');
+  if (nightMode === null) {
+    nightMode = 'false';
+  }
 
 
+
+  applyNightMode(nightMode);
+
+  document.getElementById('night-mode-toggle').addEventListener('click', () => {
+    nightMode = (nightMode === 'true') ? 'false' : 'true';
     applyNightMode(nightMode);
-
-    document.getElementById('night-mode-toggle').addEventListener('click', () => {
-        nightMode = (nightMode === 'true') ? 'false' : 'true';
-        applyNightMode(nightMode);
-        localStorage.setItem('nightMode', nightMode);
-    });
+    localStorage.setItem('nightMode', nightMode);
+  });
 });
 
-footer = document.getElementsByTagName('footer')[0]
+footer = document.getElementsByTagName('footer')[0];
 
 
 function applyNightMode(nightMode) {
-    document.body.classList.toggle("night-mode", nightMode === 'true');
-    footer.classList.toggle('night-mode-footer', nightMode === 'true')
+  document.body.classList.toggle('night-mode', nightMode === 'true');
+  footer.classList.toggle('night-mode-footer', nightMode === 'true');
 }
+  
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    displayCurrentDateTime();
+  displayCurrentDateTime();
 
-    setInterval(displayCurrentDateTime, 1000);
+  setInterval(displayCurrentDateTime, 1000);
 });
 
 function displayCurrentDateTime() {
-    const dateTimeContainer = document.getElementById('current-date-time');
-    const now = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
-    const formattedDateTime = now.toLocaleDateString('en-US', options);
+  const dateTimeContainer = document.getElementById('current-date-time');
+  const now = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+  const formattedDateTime = now.toLocaleDateString('en-US', options);
 
-    dateTimeContainer.innerHTML = `<p>${formattedDateTime}</p>`;
+  dateTimeContainer.innerHTML = `<p>${formattedDateTime}</p>`;
 }
